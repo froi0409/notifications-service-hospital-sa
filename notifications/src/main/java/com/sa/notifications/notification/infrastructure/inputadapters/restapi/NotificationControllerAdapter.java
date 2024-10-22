934c2270dec5ce53cac38f1d5782c489a6d8b244
waliray123 <willy2000.wld@gmail.com>
2024-10-21 22:02:26 -0600

    add api to controllers

--- notifications/src/main/java/com/sa/notifications/notification/infrastructure/inputadapters/restapi/NotificationControllerAdapter.java
+++ notifications/src/main/java/com/sa/notifications/notification/infrastructure/inputadapters/restapi/NotificationControllerAdapter.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/notifications")
+@RequestMapping("/api/v1/notifications")
 @WebAdapter
 public class NotificationControllerAdapter {
 
--- notifications/src/main/java/com/sa/notifications/notification/infrastructure/inputadapters/restapi/SuscribeNotificationControllerAdapter.java
+++ notifications/src/main/java/com/sa/notifications/notification/infrastructure/inputadapters/restapi/SuscribeNotificationControllerAdapter.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/notifications")
+@RequestMapping("/api/v1/notifications")
 @WebAdapter
 public class SuscribeNotificationControllerAdapter {
 
